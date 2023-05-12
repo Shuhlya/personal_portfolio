89d820e1bbc834f84aaa38876a8cd33883ee4ad3
Serhii <ua.shuhlya@gmail.com>
2023-05-12 12:35:04 +0100

    Added home.html with default configuration

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Portfolio(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='portfolio/image')
+    image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from portfolio.models import Project
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'projects': Project.objects.all()
+    }
+    return render(request, 'portfolio/home.html', context)
