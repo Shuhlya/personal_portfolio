d73fd6a83adce61912c521b0279c50d4973ddcad
Serhii <ua.shuhlya@gmail.com>
2023-05-13 09:43:25 +0100

    Added page for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -6,4 +6,12 @@ def blogs(request):
     context = {
         'blogs': Blog.objects.order_by('-id')[:4],
     }
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/all_blogs.html', context)
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/detail.html', context)
